28d42a7f1f012fa27609ed6f4dc24f95ae79d6d6
jazelsug <jazels@nevada.unr.edu>
2021-04-19 11:47:29 -0700

    added QuitMgr + updated UI panel

---
+++ Assets/QuitMgr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitMgr : MonoBehaviour
+{
+    public static QuitMgr inst;
+    private void Awake()
+    {
+        inst = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+            Application.Quit();
+    }
+}
