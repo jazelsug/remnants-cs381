ce2b4b82ad1e13db0a0aa7d456bbc08cbf9d8f66
jazelsug <jazels@nevada.unr.edu>
2021-04-18 17:44:17 -0700

    vehicle mesh collides with street

---
+++ Assets/Scripts/ControlCar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCar : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //move car based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //move vehicle based on horizontalInput
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
---
+++ Assets/Scripts/FollowVehicle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowVehicle : MonoBehaviour
+{
+    public GameObject vehicle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = vehicle.transform.position + offset;
+    }
+}
