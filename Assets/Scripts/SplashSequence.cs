44bcce9f43822bc0147f27e6405ad411439d6e91
Aisha Co <aishaysabel02@gmail.com>
2021-04-22 14:50:23 -0700

    Created splash screen
    random commit necessary

---
+++ Assets/Scripts/SplashSequence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashSequence : MonoBehaviour
+{
+    public static int SceneNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SceneNumber == 0)
+        {
+            StartCoroutine(ToGameMenu());
+        }
+    }
+
+    IEnumerator ToGameMenu()
+    {
+        yield return new WaitForSeconds(5);
+        SceneNumber = 1;
+        SceneManager.LoadScene(1);
+    }
+}
