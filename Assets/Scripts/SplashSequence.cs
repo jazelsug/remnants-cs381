667f433958e3133816519a71d3f91adf89a26879
Aisha Co <aishaysabel02@gmail.com>
2021-04-22 17:13:47 -0700

    Added Loading Bar to Splash Screen

--- Assets/Scripts/SplashSequence.cs
+++ Assets/Scripts/SplashSequence.cs
@@ -2,23 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SplashSequence : MonoBehaviour
 {
     public static int SceneNumber;
+    public GameObject loadingScreen;
+    public Slider loadingBar;
+    public Text progressText;
+
     // Start is called before the first frame update
     void Start()
     {
         if(SceneNumber == 0)
         {
-            StartCoroutine(ToGameMenu());
+            StartCoroutine(ToGameMenu(1));
         }
     }
 
-    IEnumerator ToGameMenu()
+    /**
+     * Loading MenuScene asynchronously
+     * */
+    IEnumerator ToGameMenu(int sceneIndex)
     {
-        yield return new WaitForSeconds(5);
-        SceneNumber = 1;
-        SceneManager.LoadScene(1);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingBar.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+        }
     }
 }
