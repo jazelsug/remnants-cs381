d0e83e3141ffef1a7d9e31f346b9b05b0c0fd499
jazelsug <jazels@nevada.unr.edu>
2021-04-25 16:58:06 -0700

    test devil idle to fly animation

---
+++ Assets/Scripts/ReactToVehicle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactToVehicle : MonoBehaviour
+{
+    public GameObject vehicle;
+    Animator anim;
+    public float dist;
+    public GameObject devil;
+    public Animation a;
+    public float distanceToReact = 30.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = devil.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(vehicle.transform.position, devil.transform.position);
+        if (dist < distanceToReact)
+        {
+            anim.ResetTrigger("Idle");
+            anim.SetTrigger("Fly");
+            a.Stop("devil@idle");
+            a.Play("devil@fly");
+        }
+    }
+}
---
+++ Assets/Scripts/ReactToVehicleNew.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactToVehicleNew : MonoBehaviour
+{
+    public GameObject vehicle;
+    public Animator anim;
+    public GameObject devil;
+    public float dist;
+    public float distanceToReact = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = devil.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(vehicle.transform.position, devil.transform.position);
+        if (dist < distanceToReact)
+        {
+            anim.ResetTrigger("Idle");
+            anim.SetTrigger("Fly");
+        }
+    }
+}
