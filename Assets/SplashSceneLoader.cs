e7a2f27a2cf000a0051ecb815ad89a90986119ac
jazelsug <jazels@nevada.unr.edu>
2021-05-03 09:39:48 -0700

    made splash scene script

--- Assets/SplashSceneLoader.cs
+++ Assets/SplashSceneLoader.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneMgr : MonoBehaviour
+public class SplashSceneLoader : MonoBehaviour
 {
-    /*
     public static int sceneIndex;
 
     // Start is called before the first frame update
@@ -16,34 +15,14 @@ public class SceneMgr : MonoBehaviour
             StartCoroutine(ToGameMenu());
         }
     }
-    */
+
     /**
      * Loading MenuScene asynchronously
      * */
-    /*
     IEnumerator ToGameMenu()
     {
         yield return new WaitForSeconds(5);
         sceneIndex = 1;
         SceneManager.LoadScene(sceneIndex);
-    }
-    */
-    public void PlayGame()
-    {
-        //Scene 2 is StreetScene
-        //sceneIndex = 2;
-        SceneManager.LoadScene(2);
-    }
-
-    public void AfterGameGoToMenu()
-    {
-        //Scene 1 is Menu
-        SceneManager.LoadScene(1);
-    }
-
-    public void QuitGame()
-    {
-        Debug.Log("QUIT GAME");
-        Application.Quit();
     }
 }
