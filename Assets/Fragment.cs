6f74dc77b3fd931d33c46a1a496cf84a9c3a1fba
jazelsug <jazels@nevada.unr.edu>
2021-05-09 12:00:23 -0700

    made initial fragment scripts

---
+++ Assets/Fragment.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fragment : MonoBehaviour
+{
+    public int fragmentID;
+    //static public bool collected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Reset()
+    {
+        // Give each fragment a unique ID when added to a GameObject
+        fragmentID = Random.Range(0, 10000);
+    }
+
+    /*
+     void OnCollisionEnter(Collision col)
+    {
+        // change player name for the name of your players game object
+        if (col.gameObject.name == "Car")
+        {
+            //increment kill counter
+            MonsterKillCounter.inst.count++;
+
+            //destroy devil
+            Destroy(gameObject);
+        }
+    }
+     */
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Car")
+        {
+            //add current fragment to list of collected fragments
+            FragmentMgr.inst.CollectFragment(this);
+        }
+    }
+}
---
+++ Assets/FragmentMgr.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FragmentMgr : MonoBehaviour
+{
+    static public FragmentMgr inst;
+
+    private List<int> collectedFragments;
+
+    public GameObject vehicle;
+
+    private void Awake()
+    {
+        inst = this;
+
+        collectedFragments = new List<int>();
+
+        // ensure this component will not be destroyed when switching levels
+        //DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    /*
+    private void OnLevelWasLoaded(int level)
+    {
+        var fragments = GameObject.FindObjectsOfType(typeof(Fragment));
+        foreach(Fragment f in fragments)
+        {
+            //check if we've already collected the fragment
+            if (collectedFragments.Contains(f.fragmentID))
+            {
+                //if we have collected it, then remove the object from the scene
+                Destroy(f.gameObject);
+            }
+        }
+    }
+    */
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        var fragments = GameObject.FindObjectsOfType(typeof(Fragment));
+        foreach (Fragment f in fragments)
+        {
+            //check if we've already collected the fragment
+            if (collectedFragments.Contains(f.fragmentID))
+            {
+                //if we have collected it, then remove the object from the scene
+                Destroy(f.gameObject);
+            }
+        }
+    }
+
+    public void CollectFragment(Fragment f)
+    {
+        /*
+         if (_collectedCoins.Contains(coin.CoinId))
+         {
+           Debug.LogError("You've already collected " + coin.CoinId);
+         }
+         else
+         {
+           _collectedCoins.Add(coin.CoinId);
+         }
+         GameObject.Destroy(coin.gameObject);
+     */
+
+        if (collectedFragments.Contains(f.fragmentID))
+        {
+            Debug.LogError("Already collected fragment " + f.fragmentID);
+        }
+        else
+        {
+            collectedFragments.Add(f.fragmentID);
+            Destroy(f.gameObject);
+        }
+    }
+}
